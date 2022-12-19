7a90edc6d033d5398fe7e5ebfff5f2b0be25030f
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2022-12-19 19:26:32 +0300

    fix

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ function getCookie(name) {
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 let cookiecook = getCookie("cookiecook"),
-cookiewin = document.getElementsByClassName('cookie_notice')[0];    
+cookiewin = document.getElementsByClassName('cookie__notice')[0];    
 // проверяем, есть ли у нас cookie, с которой мы не показываем окно и если нет, запускаем показ
 if (cookiecook != "no") {
   // показываем    
   cookiewin.style.display="block"; 
   // закрываем по клику
-  document.getElementById("cookie_close").addEventListener("click", function(){
+  document.getElementById("cookie__close").addEventListener("click", function(){
       cookiewin.style.display="none";    
       // записываем cookie на 1 день, с которой мы не показываем окно
       let date = new Date;
